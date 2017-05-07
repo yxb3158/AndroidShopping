d44b28bcf79b5d9fef783532d18c284455e27f9c
杨献彪 <yangxianbiao@meituan.com>
2017-05-07 13:34:08 +0800

    update .gitignore

--- target/AndroidShopping/android/js/touch.js
+++
@@ -1,6 +0,0 @@
-// window.onload = function(){
-// 		var doma=document.getElementById("wx_nav").getElementsByTagName('a');
-// 		for(var i=0;i<doma.length;i++){
-// 		doma[i].addEventListener('touchstart',function(){},false);
-// 	}
-// }
